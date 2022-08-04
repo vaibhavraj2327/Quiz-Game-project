62fe97c9e94799e9327e3abf55364289d5a7ad6e
shubham kumar <shubham.kumar@sysquare.in>
2022-08-04 18:23:05 +0530

    // the data for the questions and options and the correct answer is displayed here var data = [{question:'what is my name', opt: ['Vaibhav','Rakesh','Vimlesh'], correctAns: 'Vaibhav'}, {question:'what is my age', opt:['20','21','22'], correctAns:'21'}, {question:'where do i live', opt:['Jaipur','Jodhpur','Bikaner'], correctAns:'Jaipur'}, {question:'what am i learning now', opt:['Javascript','Reactjs','Angular'], correctAns:'Javascript'}, {question: 'From where i have completed my college', opt: ['Manipal University Jaipur','Rajasthan University','MNIT'], correctAns:'Manipal University Jaipur'}, {question: 'what course have you opted', opt: ['BCA','MCA','BBA'], correctAns: 'BCA'}]
    console.log(data)
    
    //function for the questions and options is here
    function onloading(){
        //onclicking the button the value is checked and input is stored either input is wrong or right
            //checking the answer and the input of the user in here
                //displaying if the answer is correct or not
                //ending
            //ending
        //ending
    //ending of the function
    }
    
    //function recall with post-increment

---
+++ quiz.js
@@ -0,0 +1,24 @@
+// the data for the questions and options and the correct answer is displayed here 
+var data = [{question:'what is my name', opt: ['Vaibhav','Rakesh','Vimlesh'], correctAns: 'Vaibhav'},
+{question:'what is my age', opt:['20','21','22'], correctAns:'21'},
+{question:'where do i live', opt:['Jaipur','Jodhpur','Bikaner'], correctAns:'Jaipur'},
+{question:'what am i learning now', opt:['Javascript','Reactjs','Angular'], correctAns:'Javascript'},
+{question: 'From where i have completed my college', opt: ['Manipal University Jaipur','Rajasthan University','MNIT'], correctAns:'Manipal University Jaipur'},
+{question: 'what course have you opted', opt: ['BCA','MCA','BBA'], correctAns: 'BCA'}]
+
+console.log(data)
+
+//function for the questions and options is here
+function onloading(){
+    //onclicking the button the value is checked and input is stored either input is wrong or right
+        //checking the answer and the input of the user in here
+            //displaying if the answer is correct or not
+            //ending
+        //ending     
+    //ending    
+//ending of the function
+}
+
+//function recall with post-increment
+
+
